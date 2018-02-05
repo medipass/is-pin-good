190f11b44428c1d809b459d19688b561b46daf28
jxom <jakemoxey@gmail.com>
2018-02-05 15:52:59 +1100

    Spaces > tabs lol

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ const ASCENDING_NUMBERS = '0123456789';
 const reverse = value => value.split('').reverse().join('');
 
 module.exports = input => {
-	if (typeof input !== 'string') {
-		throw new TypeError(`Expected a string, got ${typeof input}`);
-	}
+  if (typeof input !== 'string') {
+    throw new TypeError(`Expected a string, got ${typeof input}`);
+  }
 
-	if (ASCENDING_NUMBERS.includes(input) || reverse(ASCENDING_NUMBERS).includes(input)) {
-		return false;
-	}
-	if (new RegExp(`(.)\\1{${input.length - 1}}`).test(input)) {
-		return false;
-	}
+  if (ASCENDING_NUMBERS.includes(input) || reverse(ASCENDING_NUMBERS).includes(input)) {
+    return false;
+  }
+  if (new RegExp(`(.)\\1{${input.length - 1}}`).test(input)) {
+    return false;
+  }
 
-	return true;
+  return true;
 };

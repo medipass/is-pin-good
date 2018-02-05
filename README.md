f4828e8367a865fb72143a3a54a45935513d9858
jxom <jakemoxey@gmail.com>
2018-02-05 15:51:14 +1100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # is-pin-good
-🔒 Checks if a given pin code is secure. 
+
+> Checks if a given pin code is secure.
+
+
+## Install
+
+```
+$ npm install is-pin-good
+```
+
+
+## Usage
+
+```js
+const isPinGood = require('is-pin-good');
+
+isPinGood('1234');
+//=> false
+
+isPinGood('8615');
+//=> true
+
+isPinGood('5555');
+//=> false
+```
+
+
+## API
+
+### isPinGood(input)
+
+Returns a boolean.
+
+#### input
+
+Type: `string`
+
+The pin code to check.
+
+## License
+
+MIT © [Medipass Solutions](https://github.com/medipass)
---
+++ test.js
@@ -0,0 +1,32 @@
+import test from 'ava';
+import isPinGood from '.';
+
+test('it throws an Error if the input is not a string', t => {
+	const error = t.throws(() => isPinGood(1234), Error);
+	t.is(error.message, 'Expected a string, got number');
+});
+
+test('it throws an Error if the input is not given', t => {
+	const error = t.throws(() => isPinGood(), Error);
+	t.is(error.message, 'Expected a string, got undefined');
+});
+
+test('it should return false if the pin is simple (repeat numbers)', t => {
+	t.is(isPinGood('5555'), false);
+	t.is(isPinGood('6666666'), false);
+});
+
+test('it should return false if the pin is simple (ascending numbers)', t => {
+	t.is(isPinGood('3456'), false);
+	t.is(isPinGood('123456'), false);
+});
+
+test('it should return false if the pin is simple (descending numbers)', t => {
+	t.is(isPinGood('4321'), false);
+	t.is(isPinGood('98765'), false);
+});
+
+test('it should return true if the pin is complex', t => {
+	t.is(isPinGood('6126'), true);
+	t.is(isPinGood('6126123'), true);
+});
